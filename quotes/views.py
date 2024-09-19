0335089fe51b4be2a1abf601380c4cc910e43657
Haocheng Liu <easonlhc@bu.edu>
2024-09-19 16:33:32 -0400

    add changes

--- quotes/views.py
+++ quotes/views.py
@@ -5,13 +5,16 @@ import random
 quotes = [
     {"quote": "The only limit to our realization of tomorrow is our doubts of today.", "author": "Franklin D. Roosevelt"},
     {"quote": "Success is not how high you have climbed, but how you make a positive difference to the world.", "author": "Roy T. Bennett"},
-    {"quote": "In the end, we will remember not the words of our enemies, but the silence of our friends.", "author": "Martin Luther King Jr."}
+    {"quote": "In the end, we will remember not the words of our enemies, but the silence of our friends.", "author": "Martin Luther King Jr."},
+    {"quote": "The best way to predict the future is to create it.", "author": "Peter Drucker"}
 ]
+
 
 images = [
     "https://static9.depositphotos.com/1319000/1216/i/450/depositphotos_12164661-stock-photo-woman-with-arms-outstretched.jpg",
     "https://t3.ftcdn.net/jpg/02/52/65/60/240_F_252656083_CL2ijXn7hLLkL7zGnlsR1S2actQ3n9Wq.jpg",
-    "https://t3.ftcdn.net/jpg/02/69/52/30/240_F_269523005_YdDtqmGWtdu8pmnc0hgaJDZlpR9TTSHr.jpg"
+    "https://t3.ftcdn.net/jpg/02/69/52/30/240_F_269523005_YdDtqmGWtdu8pmnc0hgaJDZlpR9TTSHr.jpg",
+    "https://i0.wp.com/africanparadiseworld.com/wp-content/uploads/2021/04/nature-inspiration.jpg"
 ]
 
 #main page
